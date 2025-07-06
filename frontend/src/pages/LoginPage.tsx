78c853c053aa4e14091b39cd64a0299ac76932e2
ita-jp <28795978+ita-jp@users.noreply.github.com>
2025-07-06 14:54:50 +0900

    ログインページとトップページを行き来できるようにリンクを貼る

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
+
 export default function LoginPage() {
   return (
     <div>
       <h1>ログインページ</h1>
+      <Link to="/">トップページへ戻る</Link>
     </div>
   )
 }
--- frontend/src/pages/TopPage.tsx
+++ frontend/src/pages/TopPage.tsx
@@ -1,7 +1,10 @@
+import { Link } from 'react-router-dom'
+
 function TopPage() {
   return (
     <>
       <h1>Todo アプリ</h1>
+      <Link to="/login">ログインページへ</Link>
     </>
   )
 }
