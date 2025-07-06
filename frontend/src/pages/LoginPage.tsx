f1b5694e205fac018927068a1968c16dbbea9a8f
ita-jp <28795978+ita-jp@users.noreply.github.com>
2025-07-06 15:02:19 +0900

    ログインフォームを作る

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -5,6 +5,27 @@ export default function LoginPage() {
     <div>
       <h1>ログインページ</h1>
       <Link to="/">トップページへ戻る</Link>
+      <form>
+        <div>
+          <label htmlFor="email">メールアドレス</label>
+          <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            required 
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード</label>
+          <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            required 
+          />
+        </div>
+        <button type="submit">ログイン</button>
+      </form>
     </div>
   )
 }
