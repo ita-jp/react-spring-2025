88994148997cdabe4b5d507ff38a54f77be44454
ita-jp <28795978+ita-jp@users.noreply.github.com>
2025-07-06 14:53:22 +0900

    空のログインページを作る

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import TopPage from '../pages/TopPage'
+import LoginPage from '../pages/LoginPage'
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TopPage />} />
-        <Route path="/test" element={<TopPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   )
